5bdc0e180746e94d1506eb248dc4c6f80f284b00
Kylan <kzedy20@gmail.com>
2022-03-26 13:30:48 +0100

    Changed q3 and added annotation to q5

--- glm_1.R
+++ glm_1.R
@@ -15,7 +15,7 @@ library("ggplot2")
 library("car") # For Anova
 library("aod") # for wald.test
 library("MASS") # for glm.nb - negative binomial GLM
-
+library("DHARMa") #simulateResiduals
 # glm.RR from https://rpubs.com/kaz_yos/poisson
 glm.RR <- function(GLM.RESULT, digits = 3) {
   if (GLM.RESULT$family$family == "binomial") {
@@ -34,12 +34,12 @@ glm.RR <- function(GLM.RESULT, digits = 3) {
 }
 
 # Change your setwd
-if (Sys.info()[1] == "Windows") {
-  setwd("C:/Users/peter/My Tresors/Documentsacer/KULeuven/GLM/Project")
-} else {
-  setwd("/home/muddy/Tresors/Documentsacer/KULeuven/GLM/Project")   
-}
-
+#if (Sys.info()[1] == "Windows") {
+#  setwd("C:/Users/peter/My Tresors/Documentsacer/KULeuven/GLM/Project")
+#} else {
+#  setwd("/home/muddy/Tresors/Documentsacer/KULeuven/GLM/Project")   
+#}
+setwd("C:/Workdir/GLM/GLM_28")
 # 1. resp: The number of victims the respondent knows
 # 2. race: The race of the respondent (black or white)
 data <- read.csv("homicide.csv", header = TRUE, stringsAsFactors = TRUE)
@@ -75,16 +75,12 @@ round(exp(cbind(RR=poisFit$coefficients, confint(poisFit))), 3)
 
 # or use glm.RR - same numbers as above
 glm.RR(poisFit)
-
 # 3. Calculate the ratio of the means of the response for each race
 #   (mean response for black/mean response for white)
-
-(lambda_B <- exp(poisFit$coefficients[[1]]))
-# hmmmmmmmmmmm not quite???????????????????
-(lambda_W <- exp(poisFit$coefficients[[1]] + poisFit$coefficients[[2]]))
-# I think this needs fixing!!!!!!!!!!!!!!!!
-(ratioBW <- lambda_B/lambda_W)
-
+mrwhite <- mean(predict.glm(poisFit, data[which(data$race=="white"),]))
+mrblack <- mean(predict.glm(poisFit, data[which(data$race=="black"),]))
+ratioBW <- mrblack/mrwhite
+ratioBW #ratio = 0.27
 
 # 4. Calculate the predictions of the models for each rate (or race???)
 # Predict case per person (n = 1) 
@@ -98,31 +94,26 @@ exp(predict(poisFit, newdata = data.frame(race = "black", n = 1)))
 X2=sum(residuals(poisFit, type = "pearson")^2)
 n=dim(data)[1]
 p=length(coef(poisFit))
-data.frame(X2s=X2,pvalue=(1-pchisq(X2,n-p)))
+data.frame(X2s=X2,pvalue=(1-pchisq(X2,n-p))) #p-value not good
 
 # Deviance tests
 Dev=summary(poisFit)$deviance
 df=summary(poisFit)$df.residual
-data.frame(Dev=Dev, df=df, pvalue=(1-pchisq(Dev,df)))
+data.frame(Dev=Dev, df=df, pvalue=(1-pchisq(Dev,df))) #P-value good, deviation is relatively close to df
 
 # Likelihood Ratio Test
-Anova(poisFit, test="LR", type=3)
+Anova(poisFit, test="LR", type=3) #Looks good, significant difference between races
 
 # Wald Test
-Anova(poisFit, test="Wald", type=3)
+Anova(poisFit, test="Wald", type=3) #Looks good
 # or
 wald.test(Sigma=vcov(poisFit), b=coef(poisFit), Terms = 1:2)
 
-
-# From https://rpubs.com/kaz_yos/poisson
-#      Goodness of fit test If the residual deviance is close enough to the 
-#      residual degrees of freedom, it is a good fit. It can be tested by 
-#      Chi-squared test.
-cbind(residual.deviance           = deviance(poisFit),
-     residual.degrees.of.freedom = df.residual(poisFit),
-     chisq.p.value               = pchisq(deviance(poisFit), df.residual(poisFit), lower = F)
-)
-
+#Residuals diagnostics
+sim.pois <- simulateResiduals(poisFit,plot=T)
+hist(sim.pois)
+testUniformity(sim.pois)
+testDispersion(sim.pois) #Overdispersion issue
 
 # 6. Fit a negative binomial model and get estimated model based variances
 #    (per race) for the counts. Compare them with the observed 
